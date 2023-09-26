dfaf9c6e3aaad905a2cecb45b3fff8d7ad2e71b7
peterwakoko <peterwakoko@gmail.com>
2023-09-26 20:59:26 +0300

    second commit

--- LittleLemon/settings.py
+++ LittleLemon/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-yvqv3*%r+oh0_+0#lb&s9251et#snp$s*rfyn)8a*jvo+y+ut0"
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- LittleLemonAPI/urls.py
+++ LittleLemonAPI/urls.py
@@ -4,9 +4,9 @@ from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path("manager-view", views.manager_view),
-    path("api-token-auth", obtain_auth_token),
+    path("api/token/login", obtain_auth_token),
     path("groups/manager/users", views.manager),
-    path("groups/delivery-crew/users", views.delivery_crew),
+    path("groups/manager/users", views.delivery_crew),
     path("groups/managers", views.access_manager_group),
     path("menu-items", views.MenuItemsView.as_view()),
     path("menu-items/<int:pk>", views.SingleMenuItemView.as_view()),
